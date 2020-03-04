130e74f969b6d790524291f63f691ea14e5cd9c9
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-03-04 12:14:11 -0500

    Add basic dnd dashboard - This was supposed to be on a branch and I dun goofed.

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Board from "../components/Board";
+
+function Home() {
+  return (
+    <div>
+      <h1>Work HQ - Your Job Search Launchpad</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default Home;
