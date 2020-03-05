0034e8b120bc9f37b1826944e100bd2d144367d3
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-03-05 10:24:35 -0500

    Finish integrating Jon and Sam changes

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,35 +1,16 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Board from "../components/Board";
-import jobFetch from "../utils/jobFetch";
+import Form from "../components/Form";
 
-class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      jobList: []
-    };
-  }
-  getAllJobs = () => {
-    jobFetch.fetchAll().then(res => {
-      console.log(res.data)
-      this.setState({
-        jobList: res.data
-      });
-    });
-  };
-
-  componentDidMount() {
-    this.getAllJobs();
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Work HQ - Your Job Search Launchpad</h1>
-        <Board jobList={this.state.jobList} />
-      </div>
-    );
-  }
+function Home() {
+  const [state, setState] = useState({ newApplications: [] });
+  return (
+    <div>
+      <h1>Work HQ - Your Job Search Launchpad</h1>
+      <Form state={state} setState={setState} />
+      <Board state={state} />
+    </div>
+  );
 }
 
-export default Dashboard;
+export default Home;
