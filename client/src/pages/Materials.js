f11fae45605093029375cf081650105a8b987e84
mhitchi <mayhitchings@gmail.com>
2020-03-06 10:04:26 -0500

    added links area to Materials page

--- client/src/pages/Materials.js
+++ client/src/pages/Materials.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
 
 function Materials() {
+  const [resLinks, setResLinks] = useState([]);
+  const [covLinks, setCovLinks] = useState([]);
   const classes = {
     header: {
       background: "linear-gradient(to bottom right, #0D92FF, #18C6B3)",
@@ -15,7 +19,7 @@ function Materials() {
       paddingBottom: "10px"
     },
     resumes: {
-      height: "50px",
+      width: "100%",
       margin: "10px 10px 10px auto",
       padding: "10px auto 10px auto",
       borderRadius: "5px",
@@ -25,7 +29,7 @@ function Materials() {
       textAlign: "center",
     },
     covers: {
-      height: "50px",
+      width: "100%",
       margin: "10px auto 10px 10px",
       padding: "10px auto 10px auto",
       borderRadius: "5px",
@@ -33,7 +37,27 @@ function Materials() {
       color: "white",
       fontFamily: "'Nunito', sans-serif",
       textAlign: "center",
+    },
+    input: {
+      margin: "10px 10px 30px 10px"
+    },
+    linksCol: {
+      marginLeft: "20px"
     }
+  }
+
+  const addRes = (event) => {
+    event.preventDefault();
+    //TODO add input into db, render list
+    const { name, value } = event.target;
+    setResLinks({...resLinks, [name]: value});
+  }
+
+  const addCov = (event) => {
+    event.preventDefault();
+    //TODO add input into db, render list
+    const { name, value } = event.target;
+    setCovLinks({...covLinks, [name]: value});
   }
 
   return (
@@ -52,11 +76,56 @@ function Materials() {
         </Col>
         <Col md={3}>
           <h2 style={classes.resumes}>Resumes</h2>
-          <Button onClick="addRes">Add Link</Button>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+            <Button onClick="addRes">Add Link</Button>
+            </InputGroup.Prepend>
+            <FormControl
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-default"
+            />
+          </InputGroup>
         </Col>
         <Col md={3}>
           <h2 style={classes.covers}>Cover Letters</h2>
-          <Button onClick="addCov">Add Link</Button>
+          <InputGroup className="mb-3" style={classes.input}>
+            <InputGroup.Prepend>
+            <Button onClick="addCov">Add Link</Button>
+            </InputGroup.Prepend>
+            <FormControl
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-default"
+            />
+          </InputGroup>
+        </Col>
+        <Col md={3} style={classes.linksCol}>
+          <InputGroup className="mb-3" style={classes.input}>
+              <InputGroup.Prepend>
+              <Button onClick="addCov">Website</Button>
+              </InputGroup.Prepend>
+              <FormControl
+                aria-label="Default"
+                aria-describedby="inputGroup-sizing-default"
+              />
+            </InputGroup>
+            <InputGroup className="mb-3" style={classes.input}>
+            <InputGroup.Prepend>
+            <Button onClick="addCov">LinkedIn</Button>
+            </InputGroup.Prepend>
+            <FormControl
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-default"
+            />
+          </InputGroup>
+          <InputGroup className="mb-3" style={classes.input}>
+            <InputGroup.Prepend>
+            <Button onClick="addCov">Github</Button>
+            </InputGroup.Prepend>
+            <FormControl
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-default"
+            />
+          </InputGroup>
         </Col>
       </Row>
     </>
