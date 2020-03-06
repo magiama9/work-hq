88c8390e8b7813c7549e9276820e564c28d46c7c
Sam Randels <samrandels@gmail.com>
2020-03-06 15:46:43 -0500

    Initial Build

---
+++ client/build/precache-manifest.c47544a501cb3aa341a12ea3a9c2e0e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97aade8017577ace9b793afe05685e94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c281ddcfa989843839a5",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e4da93b767baad857ae4",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "c281ddcfa989843839a5",
+    "url": "/static/js/2.532e9d40.chunk.js"
+  },
+  {
+    "revision": "46b0fb03a333f6bc4e4cc19a524111b5",
+    "url": "/static/js/2.532e9d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4da93b767baad857ae4",
+    "url": "/static/js/main.11e7a35b.chunk.js"
+  },
+  {
+    "revision": "1f0245a0efc8f9e5cebc",
+    "url": "/static/js/runtime-main.819192b3.js"
+  }
+]);
