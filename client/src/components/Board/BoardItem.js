34ee2b3607ce8abadc577b1215573818771c8cdf
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2020-03-08 09:39:18 -0400

    edit form added to job modal with validation

--- client/src/components/Board/BoardItem.js
+++ client/src/components/Board/BoardItem.js
@@ -1,15 +1,23 @@
 import React, { useRef, useState } from "react";
 import { useDrag } from "react-dnd";
 import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 // This should be split into a separate component
 // Defines each item on the board
-const BoardItem = ({ id, children, title, company, description, url, resume, coverLetter, salary, contactEmail  }, props) => {
+const BoardItem = ({ id, children, title, company, description, url, resume, coverLetter, salary, contactEmail }, props) => {
   const [show, setShow] = useState(false);
-
+  const [validated, setValidated] = useState(false)
+  const [formMessage, setFormMessage] = useState("")
+  const [formState, setFormState] = useState({description: ""})
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const handleTyping = e => {
+    console.log("typing", e.target.value, e.target.name) // for testing //
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
 
   //make draggable
   const ref = useRef(null);
@@ -20,10 +28,38 @@ const BoardItem = ({ id, children, title, company, description, url, resume, cov
     })
   });
 
+  // Red words
+  const redStyle = {
+    color: 'red',
+  };
+
+  // For handling the save to move over to board
+  const handleSave = (event) => {
+    const form = event.currentTarget
+    // console.log("submit") // for testing
+    if (form.checkValidity() === false) {
+      // console.log("bad form") // for testing
+      setFormMessage("Please fill out the required fields")
+      event.preventDefault();
+      event.stopPropagation();
+
+    } else {
+      var oldState = props.state;
+      oldState.newApplications.push(formState);
+      props.setState({
+        ...props.state,
+        newApplications: oldState.newApplications
+      });
+      setValidated(true);
+      handleClose();
+    }
+  };
+
+
   //make transparent while dragging
   const opacity = isDragging ? 0 : 1;
   drag(ref);
-console.log(title)
+  console.log(title)
   return (
     <>
       <div ref={ref} style={{ opacity }} onClick={handleShow}>
@@ -32,17 +68,48 @@ console.log(title)
 
       {/* popup on click */}
       <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>{title} - {company}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        {description}
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="primary" onClick={handleClose}>
-          Close
-        </Button>
-      </Modal.Footer>
+        <Modal.Header closeButton>
+          <Modal.Title>{title} - {company}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {description}
+          <Form noValidate validated={validated} onSubmit={handleSave}>
+            {/* Message when required fields are not filled out  */}
+            {formMessage.length > 0 && (
+              <p style={redStyle}>{formMessage}</p>
+            )}
+            <Form.Group controlId="exampleForm.ControlInput1">
+              <Form.Label>Description <span style={redStyle}>*</span></Form.Label>
+              <Form.Control as="textarea" rows="5"
+                type="input"
+                name="description"
+                onChange={handleTyping}
+                placeholder=""
+                required="required"
+              />
+            </Form.Group>
+            <p style={redStyle}> * required</p>
+              <Button variant="primary" type="submit">
+              Save
+              </Button>
+              <span> </span>
+              <Button variant="danger" type="submit">
+              Delete
+              </Button>
+              <span> </span>
+              <Button variant="secondary" onClick={handleClose}>
+              Close
+              </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+        <Button variant="secondary">
+            Edit
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
       </Modal>
     </>
   );
