6e11351e5c92bf5c431898c743d9cf84b229a379
mhitchi <mayhitchings@gmail.com>
2020-03-09 11:03:17 -0400

    separated styles into separate files

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import classes from "./LoginStyles";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -7,36 +8,6 @@ import { FirebaseAuthProvider } from "@react-firebase/auth";
 import "firebase/auth";
 import config from "../../firebase-config";
 
-const classes = {
-  background: {
-    background: "linear-gradient(to bottom right, #0D92FF, #18C6B3)",
-    height: "100em",
-    margin: "0"
-  },
-  btnDiv: {
-    marginTop: "100px",
-    padding: "40px auto 0px auto"
-  },
-  btn: {
-    width: "100%",
-    height: "50px",
-    margin: "20px auto 0px auto",
-    backgroundColor: "white",
-    fontFamily: "'Nunito', sans-serif",
-    fontWeight: "600",
-    border: "3px solid #FFBF13",
-    borderRadius: "20px",
-    position: "block"
-  },
-  h1: {
-    fontFamily: "'Fredoka One', sans-serif",
-    fontSize: "5em",
-    color: "white",
-    textAlign: "center",
-    marginTop: "100px",
-    marginBottom: "40px"
-  }
-};
 
 function Login() {
   return (
