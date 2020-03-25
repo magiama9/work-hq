168a9acacb68281754d7e090bca7fdcf76b9723b
Sam Randels <samrandels@gmail.com>
2020-03-25 09:47:47 -0400

    Fix linting issues

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,47 +1,46 @@
-import React, { useState, useEffect } from "react";
-import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
+import React, { useState, useEffect } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
-const About = props => {
+const About = (props) => {
   const [show, setShow] = useState(false);
-  const [modalContent, setModalContent] = useState({ title: "", content: "" });
+  const [modalContent, setModalContent] = useState({ title: '', content: '' });
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const todoContent = {
-    title: "Tasks",
+    title: 'Tasks',
     content:
-
       "This is your tasks page. You can add tasks by clicking the 'New Task' button at the top of the page As you start and complete tasks, simply drag them across the columns. If you would like to edit or delete a task, simply click on the item."
   };
 
   const dashboardContent = {
-    title: "Applications",
+    title: 'Applications',
     content:
-
       "This is your dashboard! Add new jobs to your dashboard by clicking the 'New Application' button at the top of the page. As your job search progresses, simply drag your jobs across the columns. If you would like to edit or delete an application, simply click on the item."
   };
 
   const materialsContent = {
-    title: "Materials",
+    title: 'Materials',
     content:
-
       "This is your materials page. Add new resources by clicking the 'Add Resource' button at the top of the page. Materials are automatically added into the correct column as you add jobs to your dashboard. You can also drag items into different columns. If you would like to edit or delete the link, click on the card. If you would like to visit your link, click on the link text."
   };
 
-  const handleContent = page => {
+  const handleContent = (page) => {
     switch (page) {
-      case "dashboard":
+      case 'dashboard':
         setModalContent(dashboardContent);
         break;
-      case "materials":
+      case 'materials':
         setModalContent(materialsContent);
         break;
-      case "todos":
+      case 'todos':
         setModalContent(todoContent);
+        break;
+      default:
+        setModalContent(dashboardContent);
         break;
     }
   };
